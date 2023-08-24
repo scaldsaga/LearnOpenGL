48c5eaee060e5572842db433225d78c2b5ab67a4
Roman Maniuk <93196537+RomanManiuk@users.noreply.github.com>
2023-08-24 12:49:50 +0200

    grey window

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,46 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 
-int main(int, char**){
-    std::cout << "Hello, from LearnOpenGl!\n";
+int window_width = 900;
+int window_height = 600;
+
+int main()
+{
+    if (!glfwInit())
+    {
+        std::cerr << "ERROR::GLFW_INIT_ERROR\n";
+        glfwTerminate();
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *pWindow = glfwCreateWindow(window_width, window_height, "OpenGl", nullptr, nullptr);
+    if (!pWindow)
+    {
+        std::cerr << "ERROR::WINDOW_CREATION_ERROR\n";
+        glfwTerminate();
+    }
+
+    glfwMakeContextCurrent(pWindow);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cerr << "ERROR::GLAD_INIT_ERROR\n";
+        glfwTerminate();
+    }
+
+    while (!glfwWindowShouldClose(pWindow))
+    {
+        glClearColor(0.2, 0.2, 0.2, 0.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        glfwSwapBuffers(pWindow);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
 }
