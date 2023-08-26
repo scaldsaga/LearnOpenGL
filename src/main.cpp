c0c109bddbbafb9d6c42cb5c6086dc4b1e405aa7
Roman Maniuk <93196537+RomanManiuk@users.noreply.github.com>
2023-08-26 19:09:43 +0200

    added keyboard  movement

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,18 @@
 #include "render/texture.h"
 #include "render/ShaderProgram.h"
 
+void sizeCallback(GLFWwindow *window, int width, int height);
+void pressInput(GLFWwindow *window);
+
 int window_width = 900;
 int window_height = 600;
+
+float deltaTime;
+float lastFrame;
+
+glm::vec3 pos(0.0f, 0.0f, 3.0f);
+glm::vec3 front(0.0f, 0.0f, -1.0f);
+glm::vec3 up(0.0f, 1.0f, 0.0f);
 
 float vertices[] = {
     -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
@@ -57,9 +67,17 @@ float vertices[] = {
     -0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
     -0.5f, 0.5f, -0.5f, 0.0f, 1.0f};
 
-unsigned int indices[] = {
-    0, 1, 3,
-    1, 2, 3};
+glm::vec3 cubePositions[] = {
+    glm::vec3(0.0f, 0.0f, 0.0f),
+    glm::vec3(2.0f, 5.0f, -15.0f),
+    glm::vec3(-1.5f, -2.2f, -2.5f),
+    glm::vec3(-3.8f, -2.0f, -12.3f),
+    glm::vec3(2.4f, -0.4f, -3.5f),
+    glm::vec3(-1.7f, 3.0f, -7.5f),
+    glm::vec3(1.3f, -2.0f, -2.5f),
+    glm::vec3(1.5f, 2.0f, -2.5f),
+    glm::vec3(1.5f, 0.2f, -1.5f),
+    glm::vec3(-1.3f, 1.0f, -1.5f)};
 
 int main()
 {
@@ -88,22 +106,21 @@ int main()
         glfwTerminate();
     }
 
+    glEnable(GL_DEPTH_TEST);
+    glfwSetWindowSizeCallback(pWindow, sizeCallback);
+
     std::string vertexPath("D:/projects/LearnOpenGL/shaders/vertexSahder.txt");
     std::string fragmentPath("D:/projects/LearnOpenGL/shaders/fragmentShader.txt");
     ShaderProgram program(vertexPath, fragmentPath);
 
-    GLuint VBO, VAO, EBO;
+    GLuint VBO, VAO;
     glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
     glGenVertexArrays(1, &VAO);
 
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
@@ -114,20 +131,31 @@ int main()
     std::string texturePath("D:/projects/LearnOpenGL/textures/container.jpg");
     texture tex1(texturePath);
 
+    glClearColor(0.2, 0.2, 0.2, 0.0);
     while (!glfwWindowShouldClose(pWindow))
     {
-        glEnable(GL_DEPTH_TEST);
-        glClearColor(0.2, 0.2, 0.2, 0.0);
+        float currentFrame = (float)glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        pressInput(pWindow);
 
         program.use();
 
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(-45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-        program.unifomMat4fv(model, "model");
+        for (int i = 0; i < 10; i++)
+        {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            program.unifomMat4fv(model, "model");
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
 
         glm::mat4 view(1.0f);
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        view = glm::lookAt(pos, pos + front, up);
         program.unifomMat4fv(view, "view");
 
         glm::mat4 projection(1.0f);
@@ -135,13 +163,33 @@ int main()
         program.unifomMat4fv(projection, "projection");
 
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
         glfwSwapBuffers(pWindow);
         glfwPollEvents();
     }
 
     glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
     glDeleteVertexArrays(1, &VAO);
     glfwTerminate();
+}
+
+void sizeCallback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void pressInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+
+    float speed = deltaTime * 5.0f;
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        pos += front * speed;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        pos -= front * speed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        pos += glm::normalize(glm::cross(front, up)) * speed;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        pos -= glm::normalize(glm::cross(front, up)) * speed;
 }
